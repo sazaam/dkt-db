8b70c5019ccd9ad3f3ad1da1d691ffbaf874bd27
sazaam <sazaam@gmail.com>
2020-11-18 09:35:09 +0100

    hard cleanup

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '759b13f6fcfdf562706649140b6b561c'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'd0a267ce-c46a-4bea-b844-4a43314de9bd'
-};
