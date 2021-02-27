d73c967946e2823493e4ef7ed3a97953eb0e2fc4
sazaam <sazaam@gmail.com>
2021-02-27 06:52:40 +0100

    changes upon mongodb connections, can now switch in env between atlas and srv remote

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         host: env('DATABASE_HOST', process.env.DB_HOST),
-        srv: env.bool('DATABASE_SRV', true),
+        srv: env.bool('DATABASE_SRV', process.env.DB_SRV || false),
         port: env.int('DATABASE_PORT', process.env.DB_PORT),
         database: env('DATABASE_NAME', process.env.DB_NAME),
         username: env('DATABASE_USERNAME', process.env.DB_USERNAME),
@@ -16,7 +16,7 @@ module.exports = ({ env }) => ({
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: env.bool('DATABASE_SSL', process.env.DB_SSL || true),
       },
     },
   },
